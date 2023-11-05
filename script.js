15cd68ef52ad9bf61b590cf81bc87571894203d8
Abhishek <abhishekp050102@gmail.com>
2023-11-05 17:17:45 +0530

    all the features are working properly

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ let keyword="";
 async function imageSearch()
 {
     const keyword=textfieldKey.value;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${access_key}&per_page=16`;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${access_key}&per_page=20`;
     const response= await fetch(url);
     const data=await response.json();
+    if(page===1)
+    {
+        document.getElementById("image-display").innerHTML="";
+    }
     const results=data.results;
      results.map((result)=>{
         const image=document.createElement("img");
