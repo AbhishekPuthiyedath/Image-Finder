a27b5c25764d0fa18c350f24187a615a0d99d76c
Abhishek <abhishekp050102@gmail.com>
2023-11-05 15:21:47 +0530

    images displayed in screen but alignment is not correct

--- script.js
+++ script.js
@@ -11,7 +11,18 @@ async function imageSearch()
     const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${access_key}`;
     const response= await fetch(url);
     const data=await response.json();
-    console.log(data)
+    console.log(data);
+    const results=data.results;
+     results.map((result)=>{
+        const image=document.createElement("img");
+        image.src=result.urls.small;
+        const imglink=document.createElement("a");
+        imglink.href=result.links.html;
+        imglink.target="_blank"
+        imglink.appendChild(image);
+        imgDisplay.appendChild(imglink);
+
+     })
 }
     imgSearch.addEventListener("click",(e)=>{
         e.preventDefault();
