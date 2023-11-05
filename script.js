9c125793574ae2e7898714ca471d291bd6ea063c
Abhishek <abhishekp050102@gmail.com>
2023-11-05 16:53:10 +0530

    see more button added page loaging done, style edited

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const textfieldKey=document.getElementById("input_key");
 const imgSearch=document.getElementById("search-button");
 const imgDisplay=document.getElementById("image-display");
-access_key="uUFdZXkvrQDFak4-chDYhBhIdCEPVQhr_oG-509I3Kc"
+const moreButton=document.getElementById("moreButton")
+access_key="uUFdZXkvrQDFak4-chDYhBhIdCEPVQhr_oG-509I3Kc";
 
 let page=1;
 let keyword="";
 async function imageSearch()
 {
     const keyword=textfieldKey.value;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${access_key}`;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${access_key}&per_page=16`;
     const response= await fetch(url);
     const data=await response.json();
     const results=data.results;
@@ -21,11 +22,16 @@ async function imageSearch()
         imglink.target="_blank"
         imglink.appendChild(image);
         imgDisplay.appendChild(imglink);
-
+        moreButton.style.display="block";
      })
 }
     imgSearch.addEventListener("click",(e)=>{
         e.preventDefault();
         page=1;
+        imageSearch();
+    })
+    moreButton.addEventListener("click",()=>{
+        page++;
+        console.log(page);
         imageSearch();
     })
